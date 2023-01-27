44d428352a7b357ce3a50561967e52d77a32f32f
Narongkiad Namhuaithong <narongkiad1234@gmail.com>
2023-01-27 19:56:08 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue";
+</script>
+
+<style>
+table,
+th,
+td {
+  border: 1px;
+}
+</style>
+
+<template>
+  <table style="width: 100%">
+    <tr>
+      <th>
+        <div class="Picture">
+          <img src="/img/blue.jpg" alt="" width="250" />
+        </div>
+        Blue
+        <LikeSad></LikeSad>
+      </th>
+      <th>
+        <div class="Picture">
+          <img src="/img/green.jpg" alt="" width="250" />
+        </div>
+        Green
+        <LikeSad></LikeSad>
+      </th>
+      <th>
+        <div class="Picture">
+          <img src="/img/pink.jpg" alt="" width="250" />
+        </div>
+        Pink
+        <LikeSad></LikeSad>
+      </th>
+    </tr>
+    <tr>
+      <th>
+        <div class="Picture">
+          <img src="/img/red.png" alt="" width="250" />
+        </div>
+        Red
+        <LikeSad></LikeSad>
+      </th>
+      <th>
+        <div class="Picture">
+          <img src="/img/yellow.jpg" alt="" width="250" />
+        </div>
+        Yellow
+        <LikeSad></LikeSad>
+      </th>
+      <th>
+        <div class="Picture">
+          <img src="/img/black.png" alt="" width="250" />
+        </div>
+        Black
+        <LikeSad></LikeSad>
+      </th>
+    </tr>
+  </table>
+</template>
---
+++ src/components/LikeSad.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+const like = ref(0);
+const sad = ref(0);
+
+function likeClick() {
+  like.value++;
+}
+function sadClick() {
+  sad.value++;
+}
+</script>
+
+<template>
+  <div>
+    <button @click="likeClick()">
+      ปุ่ม like{{ like }}
+    </button>
+    <button @click="sadClick()">
+      ปุ่ม sad{{ sad }}
+    </button>
+  </div>
+</template>
+<style></style>
